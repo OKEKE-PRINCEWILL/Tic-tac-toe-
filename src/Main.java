8e1596dc6f548935d7b37a6e931112772bc6c0b3
OKEKE-PRINCEWILL <okesprince2005@gmail.com>
2025-01-02 11:10:35 +0100

    second commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        TicTacToe tictactoe = new TicTacToe();
+
+    }
+}
